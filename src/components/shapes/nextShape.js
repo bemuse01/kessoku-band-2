e84513062529f9c4a5104a07cdc9909a7e467cdd
bemuse01 <sequeland@gmail.com>
2024-10-21 15:45:07 +0900

    Update nextShape.js

--- src/components/shapes/nextShape.js
+++ src/components/shapes/nextShape.js
@@ -8,11 +8,11 @@ const NextShape = ({color}) => {
             preserveAspectRatio="xMidYMid meet"
         >
             <path 
-                fill-rule="evenodd"  
+                fillRule="evenodd"  
                 stroke="rgb(0, 0, 0)" 
-                stroke-width="1px" 
-                stroke-linecap="butt" 
-                stroke-linejoin="miter" 
+                strokeWidth="1px" 
+                strokeLinecap="butt" 
+                strokeLinejoin="miter" 
                 fill="rgb(0, 0, 0)"
                 d="M132.229,72.541 L65.223,116.509 C55.491,122.895 42.644,116.236 42.024,104.828 L24.223,116.509 C14.248,123.054 0.994,115.898 0.994,103.967 L0.994,16.033 C0.994,4.102 14.248,-3.054 24.223,3.491 L42.024,15.172 C42.644,3.764 55.491,-2.895 65.223,3.491 L132.229,47.459 C141.256,53.383 141.256,66.617 132.229,72.541 Z"
             />
