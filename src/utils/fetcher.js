b6772773e7f6bf94559b8717e7949e8ee2d26289
bemuse01 <sequeland@gmail.com>
2024-10-29 16:49:20 +0900

    Update fetcher.js

--- src/utils/fetcher.js
+++ src/utils/fetcher.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
 
-export default url => axios.get(url).then(res => res.data)
-// export default (...args) => fetch(...args).then(res => res.json())
+const fetcher = url => axios.get(url).then(res => res.data)
+
+export default fetcher
