21b6e0c652291d900732582aedeff6e64d8d42c8
bemuse01 <sequeland@gmail.com>
2024-11-15 03:30:00 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-        return [
-            {
-                source: '/api/:path*',
-                destination: 'http://localhost:3000/api/:path*', // 서버의 API 엔드포인트
-            },
-        ];
-    },
+    // async rewrites() {
+    //     return [
+    //         {
+    //             source: '/api/:path*',
+    //             destination: 'http://localhost:3000/api/:path*', // 서버의 API 엔드포인트
+    //         },
+    //     ];
+    // },
     images: {
         remotePatterns: [
             {
